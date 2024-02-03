d5e7d87dd349fdd2db0349b4afe00be47cde1a63
ghOst-vedant <gaikwadvedant30@gmail.com>
2024-02-03 15:52:53 +0530

    [Update]: Navbar done

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import { Link } from "react-router-dom";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 const NavigationBar = () => {
+  const [open, setOpen] = useState(false);
   return (
     <div className="pt-5 flex justify-between items-center text-white px-10 bg-transparent absolute w-[100vw]">
       <NavLink
@@ -30,11 +30,54 @@ const NavigationBar = () => {
           <NavLink activeclassname="active" to="/projects" className="navlinks">
             Profile
           </NavLink>
-          <NotificationsIcon />
+          <div
+            className=""
+            onClick={() => {
+              if (open) {
+                setOpen(false);
+              } else {
+                setOpen(true);
+              }
+            }}
+          >
+            <NotificationsIcon />
+          </div>
+          {open && (
+            <div className="absolute right-5 top-[75px] bg-black/50 rounded-xl px-5 py-4 h-[300px] w-[300px] ">
+              {events?.map((event, index) => (
+                <div key={index} className="flex flex-col ">
+                  <span className=" text-gray-300  text-xl font-medium">
+                    {event.EventName}{" "}
+                  </span>
+                  <span className="text-gray-300  text-base  ">
+                    {event.Message}{" "}
+                  </span>
+                  {index < events.length - 1 && (
+                    <hr className=" w-[100%]  self-center my-3" />
+                  )}
+                </div>
+              ))}
+              <div className="up-arrow absolute right-6 -top-3"></div>
+            </div>
+          )}
         </div>
       </div>
     </div>
   );
 };
 
+const events = [
+  {
+    EventName: "Seminar-1",
+    Message: "Seats Full",
+  },
+  {
+    EventName: "Event-2",
+    Message: "Seats Full",
+  },
+  {
+    EventName: "Event-2",
+    Message: "Seats Full",
+  },
+];
 export default NavigationBar;
