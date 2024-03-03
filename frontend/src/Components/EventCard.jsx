f654283b6372743018490cdc672f16da9cf59c7a
Aniket <aniketsahu1711@gmail.com>
2024-03-03 18:53:00 +0530

    [Refactor]:Responsiveness done for the event page desktop

--- frontend/src/Components/EventCard.jsx
+++ frontend/src/Components/EventCard.jsx
@@ -6,112 +6,112 @@ const FeaturedEvents = [
   {
     key: 1,
     eventName: "Event-1",
-    date: "13 Mar",
+    date: "14 Mar",
     category: "Technical",
-    seats: "10/15",
+    seats: "10/16",
     eventImage: eventImage,
     bgColor: "bg-[#86000A]",
   },
   {
     key: 2,
     eventName: "Event-2",
-    date: "14 Mar",
+    date: "15 Mar",
     category: "Cultural",
-    seats: "8/15",
+    seats: "8/16",
     eventImage: eventImage,
     bgColor: "bg-[#B7181B]",
   },
   {
     key: 3,
     eventName: "Events",
-    date: "14 Mar",
+    date: "15 Mar",
     category: "NSS",
-    seats: "15/30",
+    seats: "16/30",
     eventImage: eventImage,
     bgColor: "bg-[#F44B1E]",
   },
   {
     key: 4,
     eventName: "Seminar-2",
-    date: "15 Mar",
+    date: "16 Mar",
     category: "Technical",
-    seats: "15/20",
+    seats: "16/20",
     eventImage: eventImage,
     bgColor: "bg-[#FF8E00]",
   },
   {
     key: 5,
     eventName: "Seminar-3",
-    date: "15 Mar",
+    date: "16 Mar",
     category: "Technical",
-    seats: "13/30",
+    seats: "14/30",
     eventImage: eventImage,
     bgColor: "bg-[#FFB800]",
   },
   {
     key: 6,
     eventName: "Event-1",
-    date: "15 Mar",
+    date: "16 Mar",
     category: "Cultural",
-    seats: "10/15",
+    seats: "10/16",
     eventImage: eventImage,
     bgColor: "bg-[#86000A]",
   },
   {
     key: 7,
     eventName: "Event-1",
-    date: "13 Mar",
+    date: "14 Mar",
     category: "Seminar",
-    seats: "10/15",
+    seats: "10/16",
     eventImage: eventImage,
     bgColor: "bg-[#FF8E00]",
   },
   {
     key: 8,
     eventName: "Event-1",
-    date: "13 Mar",
+    date: "14 Mar",
     category: "Cultural",
-    seats: "10/15",
+    seats: "10/16",
     eventImage: eventImage,
     bgColor: "bg-[#86000A]",
   },
   {
     key: 9,
     eventName: "Seminar-2",
-    date: "15 Mar",
+    date: "16 Mar",
     category: "Seminar",
-    seats: "15/20",
+    seats: "16/20",
     eventImage: eventImage,
     bgColor: "bg-[#FF8E00]",
   },
   {
     key: 10,
     eventName: "Seminar-3",
-    date: "15 Mar",
+    date: "16 Mar",
     category: "Technical",
-    seats: "13/30",
+    seats: "14/30",
     eventImage: eventImage,
     bgColor: "bg-[#FFB800]",
   },
   {
     key: 11,
     eventName: "Event-1",
-    date: "14 Mar",
+    date: "15 Mar",
     category: "Technical",
-    seats: "10/15",
+    seats: "10/16",
     eventImage: eventImage,
     bgColor: "bg-[#86000A]",
   },
   {
     key: 12,
     eventName: "Event-1",
-    date: "13 Mar",
+    date: "14 Mar",
     category: "Cultural",
-    seats: "10/15",
+    seats: "10/16",
     eventImage: eventImage,
     bgColor: "bg-[#FF8E00]",
   },
- 
+
 ];
 
 const EventCard = ({ day, event }) => {
@@ -120,11 +120,11 @@ const EventCard = ({ day, event }) => {
     let filtered = FeaturedEvents;
 
     if (day === "Day One") {
-      filtered = filtered.filter((event) => event.date === "13 Mar");
-    } else if (day === "Day Two") {
       filtered = filtered.filter((event) => event.date === "14 Mar");
-    } else if (day === "Day Three") {
+    } else if (day === "Day Two") {
       filtered = filtered.filter((event) => event.date === "15 Mar");
+    } else if (day === "Day Three") {
+      filtered = filtered.filter((event) => event.date === "16 Mar");
     }
 
     if (event === "Cultural") {
@@ -139,7 +139,7 @@ const EventCard = ({ day, event }) => {
   }, [day, event]);
 
   return (
-    <div className="flex flex-wrap justify-center gap-4">
+    <div className="flex flex-wrap justify-center gap-10 px-10 w-[100vw]">
       {filteredEvents.map((event) => (
         <FeaturedEventCard key={event.key} {...event} />
       ))}
