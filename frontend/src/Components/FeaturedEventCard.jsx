79ce6d01d74848bc87be3de95fa51572dfc4af19
ghOst-vedant <gaikwadvedant30@gmail.com>
2024-03-07 10:20:07 +0530

    Added ktm logo and minor tweaks

--- frontend/src/Components/FeaturedEventCard.jsx
+++ frontend/src/Components/FeaturedEventCard.jsx
@@ -25,7 +25,6 @@ const FeaturedEventCard = ({
     "bg-[#FF8E00]",
     "bg-[#FFB800]",
   ];
-
   if (date === 1) {
     date = "14 Mar";
   } else if (date === 2) {
@@ -40,8 +39,9 @@ const FeaturedEventCard = ({
 
   return (
     <div
-      className={`${bgColor[index % bgColor.length]
-        } rounded-xl flex flex-col px-5 py-5 items-center sm:w-[40%] lg:w-[30%]`}
+      className={`${
+        bgColor[index % bgColor.length]
+      } rounded-xl flex flex-col px-5 py-5 items-center sm:w-[40%] lg:w-[30%]`}
       onClick={setId}
     >
       <div className="flex relative sm:w-[100%]">
@@ -51,7 +51,7 @@ const FeaturedEventCard = ({
           className="w-[100%] self-center sm:rounded-xl"
         />
         <div className=" absolute right-2  p-1 sm:p-2 z-10">
-          {teamSize < 2 ? (
+          {teamSize === 1 ? (
             <PersonIcon
               sx={{
                 position: "absolute",
@@ -74,16 +74,18 @@ const FeaturedEventCard = ({
               }}
             />
           ) : (
-            <GroupsIcon
-              sx={{
-                position: "absolute",
-                right: "0",
-                fontSize: "2rem",
-                bgcolor: "white",
-                padding: "3px",
-                borderRadius: "1rem",
-              }}
-            />
+            teamSize > 2 && (
+              <GroupsIcon
+                sx={{
+                  position: "absolute",
+                  right: "0",
+                  fontSize: "2rem",
+                  bgcolor: "white",
+                  padding: "3px",
+                  borderRadius: "1rem",
+                }}
+              />
+            )
           )}
         </div>
         <div className="absolute w-full py-2 sm:py-3 bg-black/60 backdrop-blur-sm  rounded-t-xl">
@@ -117,8 +119,8 @@ const FeaturedEventCard = ({
             {category === "c"
               ? "Cultural"
               : category === "T"
-                ? "Technical"
-                : "Seminar"}
+              ? "Technical"
+              : "Seminar"}
           </span>
         </span>
         <span className="flex flex-col items-center gap-2 text-white">
@@ -129,7 +131,9 @@ const FeaturedEventCard = ({
               color: "white",
             }}
           />
-          <span className=" font-montserat font-medium ">{seats} / {max_seats}</span>
+          <span className=" font-montserat font-medium ">
+            {seats} / {max_seats}
+          </span>
         </span>
       </div>
     </div>
--- frontend/src/Pages/About.jsx
+++ frontend/src/Pages/About.jsx
@@ -1,31 +1,39 @@
-import React from 'react'
-import navBird from "../Assets/Common_images/navbird.png"
+import React from "react";
+import navBird from "../Assets/Common_images/navbird.png";
 import redBull from "../Assets/Common_images/redBull.png";
 import monstor from "../Assets/Common_images/monstor.png";
 import adidas from "../Assets/Common_images/adidas.png";
 
 const About = () => {
-    const about = {
-        etamax: "ETAMAX is the annual techno-cultural inter college fest of FCRIT. ETAMAX indeed special fest for all Agnelites and the institution as well. Sing or dance, code or paint, write or listen, build or analyze, ETAMAX serves the best opportunities to grow and developed in the form of the workshop, competitions, games etc. ETAMAX holds a cherished spot in the hearts of our students. As a techno-cultural event, it not only serves as a platform to showcase students' intellectual progress but also transforms the campus into a lively space filled with fun, games, and the creation of enduring memories for everyone involved. Witness the rebirth of creativity as artists and innovators showcase their work, drawing inspiration from the mythical phoenix.",
-        phoenix: "This year's theme is Phoenix, symbolizing resilience and renewal with vibrant hues and sleek lines. Phoenix is a mythical bird associated with renewal and resurrection. In various cultures, it's believed to burst into flames upon death and then rise from its ashes, symbolizing eternal life and regeneration. This majestic creature is often seen as a powerful symbol of transformation, strength, and the cyclical nature of life. Experience fiery performances, visual displays, and workshops igniting creativity, culminating in a mesmerizing spectacle symbolizing renewal. Join us for a transformative and inspiring journey."
-    }
-    return (
-        <div className='flameBg bg-black flex flex-col py-24 gap-10 sm:items-center sm:py-36 sm:gap-32'>
-            <div className='flex flex-col text-white text-center px-6 gap-6 sm:w-[70%]'>
-                <h2 className='font-cinzel text-4xl font-bold sm:text-5xl'>ETAMAX</h2>
-                <p className='text-justify sm:text-lg'>{about.etamax}</p>
-                <img src={navBird} alt="bird" className='border-2 border-white rounded-xl my-4 sm:my-10 sm:w-[85%] sm:mx-auto' />
-                <h2 className='font-cinzel text-4xl font-bold sm:text-5xl'>PHOENIX</h2>
-                <p className='text-justify sm:text-lg'>{about.phoenix}</p>
-            </div>
-            <div className='flex flex-wrap justify-center gap-6 sm:gap-12'>
-                {/* sponsors */}
-                <img src={redBull} alt="sponsor" className="h-14 sm:h-24" />
-                <img src={monstor} alt="sponsor" className="h-14 sm:h-24" />
-                <img src={adidas} alt="sponsor" className="h-14 sm:h-24" />
-            </div>
-        </div>
-    )
-}
+  const about = {
+    etamax:
+      "ETAMAX is the annual techno-cultural inter college fest of FCRIT. ETAMAX indeed special fest for all Agnelites and the institution as well. Sing or dance, code or paint, write or listen, build or analyze, ETAMAX serves the best opportunities to grow and developed in the form of the workshop, competitions, games etc. ETAMAX holds a cherished spot in the hearts of our students. As a techno-cultural event, it not only serves as a platform to showcase students' intellectual progress but also transforms the campus into a lively space filled with fun, games, and the creation of enduring memories for everyone involved. Witness the rebirth of creativity as artists and innovators showcase their work, drawing inspiration from the mythical phoenix.",
+    phoenix:
+      "This year's theme is Phoenix, symbolizing resilience and renewal with vibrant hues and sleek lines. Phoenix is a mythical bird associated with renewal and resurrection. In various cultures, it's believed to burst into flames upon death and then rise from its ashes, symbolizing eternal life and regeneration. This majestic creature is often seen as a powerful symbol of transformation, strength, and the cyclical nature of life. Experience fiery performances, visual displays, and workshops igniting creativity, culminating in a mesmerizing spectacle symbolizing renewal. Join us for a transformative and inspiring journey.",
+  };
+  return (
+    <div className="flameBg bg-black flex flex-col py-24 gap-10 sm:items-center sm:py-36 sm:gap-32">
+      <div className="flex flex-col text-white text-center px-6 gap-6 sm:w-[70%]">
+        <h2 className="font-cinzel text-4xl font-bold sm:text-5xl">ETAMAX</h2>
+        <p className="text-justify sm:text-lg font-montserat">{about.etamax}</p>
+        <img
+          src={navBird}
+          alt="bird"
+          className="border-2 border-white rounded-xl my-4 sm:my-10 sm:w-[85%] sm:mx-auto"
+        />
+        <h2 className="font-cinzel text-4xl font-bold sm:text-5xl">PHOENIX</h2>
+        <p className="text-justify sm:text-lg font-montserat">
+          {about.phoenix}
+        </p>
+      </div>
+      <div className="flex flex-wrap justify-center gap-6 sm:gap-12">
+        {/* sponsors */}
+        <img src={redBull} alt="sponsor" className="h-14 sm:h-24" />
+        <img src={monstor} alt="sponsor" className="h-14 sm:h-24" />
+        <img src={adidas} alt="sponsor" className="h-14 sm:h-24" />
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
